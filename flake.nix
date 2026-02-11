d828b8062c1230ebae44822abfdda42e5a0ac993
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-02-10 22:17:16 -0800

    Adding the flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Simple tex live package";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell.override { } {
+          packages = with pkgs; [
+            pkg-config
+            libertine
+
+            texliveFull
+            harper
+          ];
+        };
+      }
+    );
+}
